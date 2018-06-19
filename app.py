9161d65a68e3d6552a78daa43c2491fe6bbb4380
Sotheara Sieng <sothearasieng@Sothearas-MacBook-Pro.local>
2018-06-19 20:40:50 +0700

    add button

--- app.py
+++ app.py
@@ -40,6 +40,8 @@ def after_send(payload, response):
     """:type payload:s fbmq.Payload"""
     print("complete")
 
+import thread_settings
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run('0.0.0.0', port)
