b74a267ad3816f59e902f00b5dffafc1528a2b9b
Sotheara Sieng <sothearasieng@Sothearas-MacBook-Pro.local>
2018-06-21 20:54:36 +0700

    add

--- thread_settings.py
+++ thread_settings.py
@@ -1,5 +1,23 @@
 from fbmq import Template
-from app import page
+from app import page, ProductConfig
+import sys
+import requests
+
+
+thread_settings_url = 'https://graph.facebook.com/v2.6/me/messenger_profile'
+token = ProductConfig.FACEBOOK_TOKEN
+
+params = {
+    "access_token": token
+}
+headers = {
+    "Content-Type": "application/json"
+}
+
+def log(str):  # simple wrapper for logging to stdout on heroku
+    print str(str)
+    sys.stdout.flush()
+
 # Greeting text
 page.greeting("Welcome to Find Me Bot! My Mission is help you to easy find the pitch. :)")
 
@@ -13,11 +31,61 @@ def start_callback(payload, event):
     page.typing_off(sender_id)
     print("Let's start! %s", sender_id)
 
-page.show_persistent_menu([Template.ButtonPostBack('Find Pitch', 'FIND_PITCH'),
-                           Template.ButtonPostBack('Reminder', 'REMINDER'),
-                           Template.ButtonPostBack('Help', 'HELP')])
+# page.show_persistent_menu([Template.ButtonPostBack('My Account', 'MY_ACCOUNT'),
+#                            Template.ButtonPostBack('News', 'NEWS'),
+#                            Template.ButtonPostBack('Help', 'HELP')])
 
-@page.callback(['FIND_PITCH'])
+persistent_menu_data = '''{
+  "persistent_menu":[
+    {
+      "locale":"default",
+      "composer_input_disabled": true,
+      "call_to_actions":[
+        {
+          "title":"My Account",
+          "type":"nested",
+          "call_to_actions":[
+            {
+              "title":"Pay Bill",
+              "type":"postback",
+              "payload":"PAYBILL_PAYLOAD"
+            },
+            {
+              "title":"History",
+              "type":"postback",
+              "payload":"HISTORY_PAYLOAD"
+            },
+            {
+              "title":"Contact Info",
+              "type":"postback",
+              "payload":"CONTACT_INFO_PAYLOAD"
+            }
+          ]
+        },
+        {
+          "type":"web_url",
+          "title":"Latest News",
+          "url":"http://www.messenger.com/",
+          "webview_height_ratio":"full"
+        }
+      ]
+    },
+    {
+      "locale":"zh_CN",
+      "composer_input_disabled":false,
+      "call_to_actions":[
+        {
+          "title":"Pay Bill",
+          "type":"postback",
+          "payload":"PAYBILL_PAYLOAD"
+        }
+      ]    
+    }
+  ]
+}'''
+requests.post(url=thread_settings_url, params=params, headers=headers, data=persistent_menu_data)
+
+@page.callback(['MY_ACCOUNT'])
 def click_persistent_menu_find_pitch(payload, event):
     sender_id = event.sender_id
     page.typing_on(sender_id)
@@ -25,7 +93,7 @@ def click_persistent_menu_find_pitch(payload, event):
     page.typing_off(sender_id)
     print("you clicked %s menu" % payload)
 
-@page.callback(['REMINDER'])
+@page.callback(['NEWS'])
 def click_persistent_menu_reminder(payload, event):
     sender_id = event.sender_id
     page.send(sender_id, "you clicked %s menu" % payload)
@@ -36,3 +104,4 @@ def click_persistent_menu_help(payload, event):
     sender_id = event.sender_id
     page.send(sender_id, "you clicked %s menu" % payload)
     print("you clicked %s menu" % payload)
+
