760318799c7dcdb0847db90cf456ed6fc42d9137
Sotheara Sieng <sothearasieng@Sothearas-MacBook-Pro.local>
2018-06-22 10:15:38 +0700

    add

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 class ProductConfig():
     FACEBOOK_TOKEN = "EAAG8ZCCEzUlEBAI5DiewsBvJrd3kADU8dCZAZBP6X5am7ZBrsV4G3pLIAnZACZAQSv8L6EHjiIPP2ZCdNhxmQ3hhSrVrNhUhMgskbMdkMtSnQyp9gfPhrFyuW1j12L5CZC2Pgc6sqNOjHT4OZBqXf4Mmr5sHOGnKCf1sCDKZCI0BUdFgZDZD"
     VERIFY_TOKEN = "ebot messenger$$"
+
+class DevelopmentConfig():
+    PAGE_TOKEN = ''
