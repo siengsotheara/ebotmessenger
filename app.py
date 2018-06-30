b02f484a8c99f49facd078727d249251b2af0dfe
Sotheara Sieng <sothearasieng@Sothearas-MacBook-Pro.local>
2018-06-30 20:15:16 +0700

    add

--- app.py
+++ app.py
@@ -3,8 +3,61 @@ from flask import Flask, request, render_template, redirect, url_for
 from fbmq import Page, Template
 from config import ProductConfig
 
+from flask_material import Material  
+from flask_wtf import Form, RecaptchaField
+from flask_wtf.file import FileField
+from wtforms import TextField, HiddenField, ValidationError, RadioField,BooleanField, SubmitField, IntegerField, FormField, validators
+from wtforms.validators import Required
+
+
 page = Page(ProductConfig.FACEBOOK_TOKEN)
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'USE-YOUR-OWN-SECRET-KEY-DAMNIT'
+app.config['RECAPTCHA_PUBLIC_KEY'] = 'TEST'
+
+
+Material(app)
+
+# straight from the wtforms docs:
+class TelephoneForm(Form):
+    country_code = IntegerField('Country Code', [validators.required()])
+    area_code = IntegerField('Area Code/Exchange', [validators.required()])
+    number = TextField('Number')
+
+class ExampleForm(Form):
+    field1 = TextField('First Field', description='This is field one.')
+    field2 = TextField('Second Field', description='This is field two.',
+                       validators=[Required()])
+    hidden_field = HiddenField('You cannot see this', description='Nope')
+    recaptcha = RecaptchaField('A sample recaptcha field')
+    radio_field = RadioField('This is a radio field', choices=[
+        ('head_radio', 'Head radio'),
+        ('radio_76fm', "Radio '76 FM"),
+        ('lips_106', 'Lips 106'),
+        ('wctr', 'WCTR'),
+    ])
+    checkbox_field = BooleanField('This is a checkbox',
+                                  description='Checkboxes can be tricky.')
+
+    # subforms
+    mobile_phone = FormField(TelephoneForm)
+
+    # you can change the label as well
+    office_phone = FormField(TelephoneForm, label='Your office phone')
+
+    ff = FileField('Sample upload')
+
+    submit_button = SubmitField('Submit Form')
+
+
+    def validate_hidden_field(self, form, field):
+        raise ValidationError('Always wrong')
+
+@app.route('/hello')
+def hello():
+    form = ExampleForm()   
+    return render_template('test.html', form = form) 
 
 @app.route('/')
 def index():
@@ -39,6 +92,7 @@ def handle_message(event):
 @page.after_send
 def after_send(payload, response):
     print response
+
 
 import thread_settings
 
